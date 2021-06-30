d07e9e3d175d094fb1ca4cf747c265614cfd28c3
aurelie-castro <acdemarly@gmail.com>
2021-06-30 12:26:09 +0200

    changed pattern

--- js/game.js
+++ js/game.js
@@ -74,7 +74,7 @@ function preload() {
     this.load.image('star', './assets/yellow-star.png');
     
     //---background pattern---
-    this.load.image('gameBg', './assets/feuilledroite-01-01.png');
+    this.load.image('gameBg', './assets/leaf--pattern-01.png');
 
 }
 
